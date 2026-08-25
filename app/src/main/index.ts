92e5f30a916e22a3566cc6348b219a1dd2cacc4e
LukeMyb <ryutten@icloud.com>
2026-08-26 05:59:33 +0900

    fix: YouTubeプレビューのBot判定エラーをキャッシュクリアで回避

--- app/src/main/index.ts
+++ app/src/main/index.ts
@@ -85,6 +85,9 @@ function createWindow(): void {
 app.whenReady().then(() => {
   electronApp.setAppUserModelId('com.yt2gif.app')
 
+  // アプリ起動時に内部のキャッシュやクッキーを完全に消去し、Bot判定を解除する
+  session.defaultSession.clearStorageData()
+
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
