e96d89473210913d07172603b3d828403bbd7735
LukeMyb <ryutten@icloud.com>
2026-08-26 02:59:39 +0900

    chore: 開発者ツールを非表示に

--- app/src/main/index.ts
+++ app/src/main/index.ts
@@ -37,7 +37,7 @@ function createWindow(): void {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
